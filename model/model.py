5bdfc5b04ddc8b6361056d4476af3d97fe277016
Fabio <fabiopires1011@gmail.com>
2025-01-07 19:03:45 +0000

    Updated template

--- model/model.py
+++ model/model.py
@@ -499,18 +499,14 @@ def display_team_matchup(visitor_team_name, home_team_name, visitor_logo_url, ho
         <div style='display: flex; align-items: center; justify-content: center;'>
             <div style='text-align: center; margin-right: 30px;'>
                 <img src='data:image/png;base64,{visitor_logo_base64}' width='100' height='100' style='display: block; margin: auto;' />
-                                <div><strong>{visitor_team_name} 
-                (<span style='color: darkgreen;'>{data.AWAY_TEAM_TOTAL_WINS}W</span>-<span style='color: red;'>{AWAY_TEAM_TOTAL_LOSSES}L</span>)</strong>
-            </div>
+                <div><strong>{visitor_team_name}</strong></div>
             </div>
             <div style='text-align: center; margin: 0 30px; font-size: 32px;'>
                 @
             </div>
             <div style='text-align: center; margin-left: 30px;'>
                 <img src='data:image/png;base64,{home_logo_base64}' width='100' height='100' style='display: block; margin: auto;' />
-                 <div><strong>{home_team_name} 
-                (<span style='color: darkgreen;'>{data.HOME_TEAM_TOTAL_WINS}W</span>-<span style='color: red;'>{HOME_TEAM_TOTAL_LOSSES}L</span>)</strong>
-            </div>
+                <div><strong>{home_team_name}</strong></div>
             </div>
         </div>
         <div style='text-align: center; margin-top: 20px;'>
