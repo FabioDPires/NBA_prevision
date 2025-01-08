b3c720f243dc4f6d9382b5c31b2fd91f1e83af30
Fabio <fabiopires1011@gmail.com>
2025-01-08 14:55:33 +0000

    Changes to layout

--- model/model.py
+++ model/model.py
@@ -503,8 +503,8 @@ def display_team_matchup(visitor_team_name, home_team_name, visitor_logo_url, ho
                 <div>
                 (<span style='color: darkgreen;'>{data["AWAY_TEAM_TOTAL_WINS"]}W</span>
                 <span>-</span>
-                </div>
                 <span style='color: red;'>{data["AWAY_TEAM_TOTAL_LOSSES"]}L</span>)</strong>
+                </div>
             </div>
                         <div>Won {data["AWAY_TEAM_WIN_PERCENTAGE_AWAY"]*100:.1f}% of the games on the road </span>
             </strong></div>
@@ -515,9 +515,11 @@ def display_team_matchup(visitor_team_name, home_team_name, visitor_logo_url, ho
             <div style='text-align: center; margin-left: 30px;'>
                 <img src='data:image/png;base64,{home_logo_base64}' width='100' height='100' style='display: block; margin: auto;' />
                             <div><strong>{home_team_name} 
+                            <div>
                 (<span style='color: darkgreen;'>{data["HOME_TEAM_TOTAL_WINS"]}W</span>
                 <span>-</span>
                 <span style='color: red;'>{data["HOME_TEAM_TOTAL_LOSSES"]}L</span>)</strong>
+                </div>
             </div>
             <div>Won {data["HOME_TEAM_WIN_PERCENTAGE_AT_HOME"]*100:.1f}% of the games at home </span>
             </strong></div>
